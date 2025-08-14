f585bdd081603fdc8839459ae984b27f4796895a
Harrison LaBollita <harrisonlabollita@gmail.com>
2025-08-14 13:18:09 -0400

    add integer access to BlockGf

--- python/triqs/gf/block_gf.py
+++ python/triqs/gf/block_gf.py
@@ -269,21 +269,32 @@ class BlockGf:
 
     #--------------  Bracket operator []  -------------------------
 
-    def __getitem__(self,key):
+    # string access
+    def __getitem_str(self,key):
         try:
             g = self.__me_as_dict[key]
         except KeyError:
             raise IndexError("bloc index '" + repr(key) + "' incorrect. Possible indices are: "+ repr(self.__indices))
         return g
 
+    # integer access
+    def __getitem_int(self, key):
+        if key < 0 or key >= self.n_blocks:
+            raise IndexError(f"bloc index {key} is out of bounds. Possible indices are: {list(range(self.n_blocks))}")
+        return self.__GFlist[key]
+
+    def __getitem__(self, key):
+        if isinstance(key, str): return self.__getitem_str(key)
+        elif isinstance(key, int): return self.__getitem_int(key)
+        else:
+            raise TypeError(f"BlockGf can be accessed using str or int indices, not {type(key)}")
+
+
     def __setitem__(self,key,val):
         if key == slice(None, None, None): # G[:] = XXX is the same as G << XXX
             self << val
             return
-        try:
-            g = self.__me_as_dict[key]
-        except KeyError:
-            raise IndexError("bloc index '" + repr(key) + "' incorrect. Possible indices are: "+ repr(self.__indices))
+        g = self[key]
         g << val
 
     # -------------- Various operations -------------------------------------
